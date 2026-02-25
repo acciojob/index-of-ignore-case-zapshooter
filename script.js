796b814c89c7584b2a1e8d09e88e3c3e2fc8db00
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-02-25 23:28:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  return s1.toLowerCase().indexOf(s2.toLowerCase());
 }
-
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
